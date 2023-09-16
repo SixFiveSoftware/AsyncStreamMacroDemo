7b2f73a9f08324e32e16115e181b2fa700547507
BJ Miller <bjmiller@mirror.co>
2023-09-16 09:59:43 -0400

    Initial Commit

---
+++ AsyncScreamMacroDemo/AsyncScreamMacroDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncScreamMacroDemoApp.swift
+//  AsyncScreamMacroDemo
+//
+//  Created by BJ Miller on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncScreamMacroDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
